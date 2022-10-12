41b79f68c9bc5bf7c471330e428b9b87385b21df
Rafael Artioli <rafael_artioli@hotmail.com>
2022-10-12 19:23:02 -0300

    Aplicado programação funciona

--- app/main.py
+++ app/main.py
@@ -1,11 +1,20 @@
-peso = int(input("Digite seu peso :"))
-altura = float(input("Digite sua altura :"))
+# coleta de informação
+def coleta_informacao():
 
+    peso = int(input("Digite seu peso :"))
+    altura = float(input("Digite sua altura :"))
+    return peso,altura
 
-imc = peso/(altura*altura) 
+# calculo imc
+def calculo_imc(peso,altura):
+    imc = peso/(altura*altura) 
+    return imc
 
+informacoes = coleta_informacao()
 
+peso = informacoes[0]
+altura = informacoes[1]
 
+print(calculo_imc(peso,altura))
 
-print(imc)
 
